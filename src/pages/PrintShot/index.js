d2aecbbca2edf45f6d43e34006e7a68f65f53dfc
Sutolix <tg884ght@gmail.com>
2021-03-06 17:36:15 -0300

    heroku api integration

--- src/pages/PrintShot/index.js
+++ src/pages/PrintShot/index.js
@@ -7,10 +7,12 @@ import api from '../../services/api'
 export default function PrintShot() {
 
   const [imageData, setImageData] = useState('')
+  const [awaitingQuery, setAwaitingQuery] = useState(true)
   const [loadImage, setLoadImage] = useState(true)
+  const [siteUrl, setSiteUrl] = useState('')
   
-  async function printGenerator() {
-    const response = await api.get('/print')
+  async function printGenerator(url) {
+    const response = await api.post('/print', url)
 
     const imageBase64 = response.data.ImageBase64
 
@@ -18,15 +20,33 @@ export default function PrintShot() {
     setLoadImage(false)
   }
 
-  printGenerator()
+  function handleQuery(e){
+		e.preventDefault();
+
+    printGenerator({
+      url: siteUrl
+    })
+    setAwaitingQuery(false)
+	}
 
   return (
     <div className="container">
-      {loadImage
-      ? <div>Carregando Image</div>
-      : <Base64Downloader base64={imageData} downloadName="screenshot">
-          Click para baixar
-        </Base64Downloader>}
+      <form onSubmit={handleQuery}>
+      	<label>Link do site</label>
+      	<input
+      		type="text"
+      		value={siteUrl}
+      		onChange={e => setSiteUrl(e.target.value)}
+      	/>
+        <button type="submit">Gerar</button>
+      </form>
+      {awaitingQuery ? <></> : 
+        loadImage
+        ? <div>Carregando Image</div>
+        : <Base64Downloader base64={imageData} downloadName="screenshot">
+            Click para baixar
+          </Base64Downloader>
+      }
 
     </div>
   )
